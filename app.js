4fcef9f5c889bb817cb3df7857253eecf72be635
shebl <shebblloll@gmail.com>
2023-02-22 18:47:14 +0200

    finish the functions and app

--- app.js
+++ app.js
@@ -20,17 +20,54 @@ for (let i = 1; i <= imgsCount; i++) {
 document.querySelector(".indicators").appendChild(bullets);
 ////////////////////////////////////////////////////////////////////////
 let bullet = document.querySelector("#bullets-ul");
+bulletss = Array.from(document.querySelectorAll("#bullets-ul li"));
 ////////////////////////////////////////////////////////////////////////
-
+bulletss.forEach((bullet) => {
+  bullet.onclick = function () {
+    firstImg = bullet.innerHTML;
+    checker();
+  };
+});
 ////////////////////////////////////////////////////////////////////////
-function nextSlide() {}
+function nextSlide() {
+  if (next.classList.contains("disable") == true) {
+    return false;
+  } else {
+    firstImg++;
+    checker();
+    // console.log(firstImg);
+  }
+}
 function prevSlide() {
-  console.log("prev");
+  if (previous.classList.contains("disable") == true) {
+    return false;
+  } else {
+    firstImg--;
+    checker();
+    // console.log(firstImg);
+  }
 }
 function checker() {
   slideNumber.innerHTML = `slide #${firstImg} of ${imgsCount}`;
+  imgs.forEach((img) => {
+    img.classList.remove("active");
+  });
   imgs[firstImg - 1].classList.add("active");
+  bulletss.forEach((bullet) => {
+    bullet.classList.remove("active");
+  });
   bullet.children[firstImg - 1].classList.add("active");
+
+  if (firstImg == 1) {
+    previous.classList.add("disable");
+  } else {
+    previous.classList.remove("disable");
+  }
+  if (firstImg == imgsCount) {
+    next.classList.add("disable");
+  } else {
+    next.classList.remove("disable");
+  }
 }
 ////////////////////////////////////////////////////////////////////////
 checker();
