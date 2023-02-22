64335d7ca262c95a1c879471030405c15457ddde
shebl <shebblloll@gmail.com>
2023-02-22 18:22:43 +0200

    add bullets

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ let firstImg = 1;
 next.onclick = nextSlide;
 previous.onclick = prevSlide;
 ///////////////////////////////////////////////////////////////////
-
+let bullets = document.createElement("ul");
+bullets.setAttribute("id", "bullets-ul");
+for (let i = 1; i <= imgsCount; i++) {
+  let liTag = document.createElement("li");
+  liTag.setAttribute("data-index", i);
+  liTag.appendChild(document.createTextNode(i));
+  bullets.appendChild(liTag);
+}
+document.querySelector(".indicators").appendChild(bullets);
 ////////////////////////////////////////////////////////////////////////
 function nextSlide() {
   console.log("next");
